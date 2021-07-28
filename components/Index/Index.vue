3dbbf6a2ac7cbdb297eadcc58d59f4d8bc1ef00b
tukangpel <soefyan45@gmail.com>
2021-07-28 23:18:27 +0700

    oke

--- components/Index/Index.vue
+++ components/Index/Index.vue
@@ -1,6 +1,35 @@
 <template>
-<div>
-  
+<div class="h-screen w-screen bg-gray-100">
+  <div class="h-24 row w-screen items-center py-2">
+    <div class="h-18 lg:h-20 w-11/12 lg:w-8/12 flex rounded-full bg-white mx-auto justify-between py-2 px-8 items-center">
+      <div class="w-6/12 flex h-full justify-star">
+        <div class="w-3/12 h-full items-center flex">
+          <!-- <img src="https://gbckelxngttdredvrsfh.supabase.in/storage/v1/object/public/assets/design/link-up.png" class="object-fit "> -->
+          <h1 class="text-2xl lg:text-4xl font-semibold text-blue-600 my-auto mx-2">LinkUp</h1>
+        </div>
+        <div class="w-9/12 hidden lg:block h-full space-x-4 px-2 flex">
+          <span class="text-2xl text-gray-800 my-auto">FAQ</span>
+          <span class="text-2xl text-gray-800 my-auto">Help</span>
+        </div>
+      </div>
+      <div class="w-6/12 h-full hidden lg:block justify-end space-x-4 px-2 items-center flex">
+        <span class="text-2xl text-gray-800 my-auto">Login</span>
+        <span class="text-2xl text-white font-semibold py-2 px-4 rounded-full text-center my-auto bg-gray-800">Singup</span>
+      </div>
+    </div>
+  </div>
+  <div class="w-screen pt-20 lg:pt-24 flex items-center">
+    <div class="row my-12 mx-auto items-center">
+      <h1 class="text-black font-bold mx-auto text-center text-5xl lg:heading-font">Masih Proses Pembuatan Sabar Ya</h1>
+      <!-- <h1 class="text-black font-bold mx-auto text-center text-5xl lg:heading-font">All In One Link</h1> -->
+      <div class="text-xl lg:text-5xl text-center">
+        We make sure your followes can quickly
+      </div>
+      <div class="text-xl lg:text-5xl text-center">
+         find what they're looking for
+      </div>
+    </div>
+  </div>
 </div>
 </template>
 
@@ -11,5 +40,7 @@ export default {
 </script>
 
 <style>
-
+.heading-font{
+  font-size: 180px;
+}
 </style>
